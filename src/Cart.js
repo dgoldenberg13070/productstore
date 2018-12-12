c915aeb3ecd00ee7a7f8e3ddcdf74a10f617b687
dgoldenberg13070 <45080317+dgoldenberg13070@users.noreply.github.com>
2018-12-12 13:10:59 -0600

    d
    d

--- src/Cart.js
+++ src/Cart.js
@@ -1,10 +1,46 @@
 import React, { Component } from 'react';
 
+import { connect } from 'react-redux';
+import { addToCart } from './actions';
+import { removeFromCart } from './actions';
+
 class Cart extends Component {
   render() {
-    return (<div>Cart Works!</div>);
+    return (
+      <ul className="list-group">
+        <li className="list-group-item d-flex justify-content-between align-items-center">
+          Hot Dog
+          <span className="badge badge-primary badge-pill">14</span>
+          <span className="badge badge-primary badge-pill">14</span>
+        </li>
+        <li className="list-group-item d-flex justify-content-between align-items-center">
+          Ice Cream
+          <span className="badge badge-primary badge-pill">2</span>
+          <span className="badge badge-primary badge-pill">2</span>
+        </li>
+        <li className="list-group-item d-flex justify-content-between align-items-center">
+          Root Beer
+          <span className="badge badge-primary badge-pill">1</span>
+          <span className="badge badge-primary badge-pill">1</span>
+        </li>
+      </ul>
+    );
   }
 }
 
-export default Cart;
+const mapStateToProps = (state) => {
+  return {
+    cart: state.cartReducer
+  }
+}
+
+const mapDispatchToProps = (dispatch, ownProps) => {
+  return {
+    addToCart: (product) => {
+      dispatch(addToCart(product))
+    }
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Cart);
 
